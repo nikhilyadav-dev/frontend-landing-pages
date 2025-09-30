6662cd74fafb6d864ee384e9f4636740d38c48af
Nikhil Yadav <nikhilyadav.prof@gmail.com>
2025-09-30 23:18:14 +0530

    feat: add Hero Section and Styling

--- urbannest-landing-page/script.js
+++ urbannest-landing-page/script.js
@@ -1,8 +1,12 @@
 const navToggler = document.querySelector("[data-nav-toggler]");
 const navbar = document.querySelector(".navbar");
-console.log(navbar);
+const header = document.querySelector(".header");
 
 navToggler.addEventListener("click", () => {
   navToggler.classList.toggle("active");
   navbar.classList.toggle("active");
+});
+
+window.addEventListener("scroll", (e) => {
+  header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
