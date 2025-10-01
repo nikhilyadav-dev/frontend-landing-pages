6416b72348897cc3a0b974c47945967db36401e6
Nikhil Yadav <nikhilyadav.prof@gmail.com>
2025-10-01 23:07:30 +0530

    feat: add Property section and Crad styling

--- urbannest-landing-page/script.js
+++ urbannest-landing-page/script.js
@@ -1,6 +1,8 @@
 const navToggler = document.querySelector("[data-nav-toggler]");
 const navbar = document.querySelector(".navbar");
 const header = document.querySelector(".header");
+const favBtns = document.querySelectorAll(".fav-btn");
+console.log(favBtns);
 
 navToggler.addEventListener("click", () => {
   navToggler.classList.toggle("active");
@@ -10,3 +12,17 @@ navToggler.addEventListener("click", () => {
 window.addEventListener("scroll", (e) => {
   header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+for (let btn of favBtns) {
+  btn.addEventListener("click", () => {
+    console.log(btn);
+    btn.classList.toggle("active");
+    console.log("clicked");
+  });
+}
+
+// favBtns.forEach(($toggleBtn) => {
+//   $toggleBtn.addEventListener("click", () => {
+//     $toggleBtn.classList.toggle("active");
+//   });
+// });
